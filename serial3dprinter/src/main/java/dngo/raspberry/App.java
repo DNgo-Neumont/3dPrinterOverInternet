e49f8e02571fdd772f3021f56cfe94c1303877b7
DNgo-Neumont <dngo@student.neumont.edu>
2022-06-29 23:46:24 -0600

    Testing an event listener in place of the while loop.

--- serial3dprinter/src/main/java/dngo/raspberry/App.java
+++ serial3dprinter/src/main/java/dngo/raspberry/App.java
@@ -40,22 +40,46 @@ public class App
             e.printStackTrace();
         }
 
-        SerialPort portSelected = ports[choice];
+        final SerialPort portSelected = ports[choice];
 
         portSelected.setBaudRate(250000);
         portSelected.openPort();
         portSelected.setComPortTimeouts(SerialPort.TIMEOUT_SCANNER, 0, 0);
-        BufferedReader portReader = new BufferedReader(new InputStreamReader(portSelected.getInputStream()));
+        portSelected.addDataListener(new SerialPortDataListener() {
+
+            @Override
+            public int getListeningEvents() {
+                return SerialPort.LISTENING_EVENT_DATA_AVAILABLE;
+            }
+
+            @Override
+            public void serialEvent(SerialPortEvent event) {
+                if(event.getEventType() != SerialPort.LISTENING_EVENT_DATA_AVAILABLE){
+                    return;
+                }    
+
+                byte[] dataRec = new byte[portSelected.bytesAvailable()];
+
+                int read = portSelected.readBytes(dataRec, dataRec.length);
+
+                System.out.println(new String(dataRec));
+                
+                //BufferedReader portReader = new BufferedReader(new InputStreamReader(portSelected.getInputStream()));
+
+            }
+            
+        });
+
         
 
-        try{
-            String lineContent;
-            while((lineContent = portReader.readLine()) != null && !lineContent.isEmpty() && portSelected.bytesAvailable() > 0){//((lineContent = portReader.readLine()) != null && !lineContent.equals("\n")){
-                System.out.println(lineContent);
-            }
-        } catch(Exception e){
-            e.printStackTrace();
-        }
+        // try{
+        //     String lineContent;
+        //     while((lineContent = portReader.readLine()) != null && !lineContent.isEmpty() && portSelected.bytesAvailable() > 0){//((lineContent = portReader.readLine()) != null && !lineContent.equals("\n")){
+        //         System.out.println(lineContent);
+        //     }
+        // } catch(Exception e){
+        //     e.printStackTrace();
+        // }
         portSelected.closePort();
 
         System.out.println( "Hello World!" );
