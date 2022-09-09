9009bca1d9a21a8402428691fa5015f31e5eddf5
DNgo-Neumont <dngo@student.neumont.edu>
2022-09-09 15:12:23 -0600

    Trying to fix the if statement

--- serial3dprinter/src/main/java/dngo/raspberry/GcodeListener.java
+++ serial3dprinter/src/main/java/dngo/raspberry/GcodeListener.java
@@ -75,7 +75,7 @@ public class GcodeListener implements SerialPortDataListener{
 
         System.out.println(response);
 
-        if(response.equals("ok")){
+        if(response.contentEquals("ok")){
             try {
                 currentLine = gcodeReader.readLine();
 
