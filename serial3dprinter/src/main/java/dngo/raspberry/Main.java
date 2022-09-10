0923f2fb81960a3bb2fb01ef2f9a5f37f6407cc8
DNgo-Neumont <dngo@student.neumont.edu>
2022-09-10 12:42:32 -0600

    Messing around with timeouts again.

--- serial3dprinter/src/main/java/dngo/raspberry/Main.java
+++ serial3dprinter/src/main/java/dngo/raspberry/Main.java
@@ -39,7 +39,7 @@ public class Main {
                 portSelected.setBaudRate(baudRate);
 
                 portSelected.openPort();
-                portSelected.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0);
+                portSelected.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 1000, 0);
 
                 System.out.println("Giving the printer a second to initialize the connection.");
                 Thread.sleep(1000);
