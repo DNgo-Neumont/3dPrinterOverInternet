64655ed2f4e0f06cae9b2d81adefa3fcb20ece25
DNgo-Neumont <dngo@student.neumont.edu>
2022-09-11 13:54:14 -0600

    forgot to add blank line checks. Whoops.

--- serial3dprinter/src/main/java/dngo/raspberry/GcodeListener.java
+++ serial3dprinter/src/main/java/dngo/raspberry/GcodeListener.java
@@ -59,15 +59,15 @@ public class GcodeListener implements SerialPortDataListener{
     public void sendFirst() throws IOException{
         currentLine = gcodeReader.readLine();
 
-        // if(currentLine.isBlank()){
-        //     while(currentLine != null && currentLine.isBlank()){      
-        //         currentLine = gcodeReader.readLine();
-        //     }
-        // }else if(currentLine.charAt(0) == ';'){
-        //     while(currentLine != null && currentLine.charAt(0) == ';'){
-        //         currentLine = gcodeReader.readLine();
-        //     }
-        // }
+        if(currentLine.isBlank()){
+            while(currentLine != null && currentLine.isBlank()){      
+                currentLine = gcodeReader.readLine();
+            }
+        }else if(currentLine.charAt(0) == ';'){
+            while(currentLine != null && currentLine.charAt(0) == ';'){
+                currentLine = gcodeReader.readLine();
+            }
+        }
 
 
         currentLine = currentLine + "\n";
@@ -151,16 +151,15 @@ public class GcodeListener implements SerialPortDataListener{
 
                 System.out.println(currentLine);
 
-                //Will test more tomorrow - For now will see if recompiled gcode fixes printer shifting issue
-                // if(currentLine.isBlank()){
-                //     while(currentLine != null && currentLine.isBlank()){      
-                //         currentLine = gcodeReader.readLine();
-                //     }
-                // }else if(currentLine.charAt(0) == ';'){
-                //     while(currentLine != null && currentLine.charAt(0) == ';'){
-                //         currentLine = gcodeReader.readLine();
-                //     }
-                // }
+                if(currentLine.isBlank()){
+                    while(currentLine != null && currentLine.isBlank()){      
+                        currentLine = gcodeReader.readLine();
+                    }
+                }else if(currentLine.charAt(0) == ';'){
+                    while(currentLine != null && currentLine.charAt(0) == ';'){
+                        currentLine = gcodeReader.readLine();
+                    }
+                }
 
                 if(currentLine == null){
                     System.out.println("print complete");
