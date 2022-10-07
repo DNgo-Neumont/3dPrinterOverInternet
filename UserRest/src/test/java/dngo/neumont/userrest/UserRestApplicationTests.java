862bbfd351acbe440e8c65ac5d55fb02ed1ae5a2
DNgo-Neumont <dngo@student.neumont.edu>
2022-10-06 18:08:41 -0600

    Start of user REST api

---
+++ UserRest/src/test/java/dngo/neumont/userrest/UserRestApplicationTests.java
@@ -0,0 +1,13 @@
+package dngo.neumont.userrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
