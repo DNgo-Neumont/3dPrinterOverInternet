1445b55049aac02c5de613e42837db299a5cd45c
DNgo-Neumont <78094165+DNgo-Neumont@users.noreply.github.com>
2022-12-02 11:29:59 -0700

    fixed a slight issue with queuing print generation

--- DjangoFrontend/Scripts/mainsite/static/mainapp/js/dashboard.js
+++ DjangoFrontend/Scripts/mainsite/static/mainapp/js/dashboard.js
@@ -32,7 +32,7 @@ function queuePrintCallback(){
 
     var selectItems = document.createElement("SELECT");
     selectItems.className = "printer_files"
-    selectItems.id = "printer_file_select"
+    // selectItems.id = "printer_file_select"
 
     fileRequest.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 200){
@@ -195,7 +195,7 @@ function queuePrintCallback(){
                         displayBox.append(ackMessage);
                     })
                     printerName.innerHTML = printerList[i];
-            
+                    selectItems.id = "printer_file_select" + "_" + printerList[i];
                     printerDiv.appendChild(printerName);
                     printerDiv.appendChild(selectItems);
                     printerDiv.appendChild(printerButton);
